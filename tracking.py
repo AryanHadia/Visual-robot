9fa71bdfb5b95a49beb081757c631a12522639f0
AryanHadia <aryan.hd09@gmail.com>
2026-07-06 02:10:37 +0330

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -1,12 +1,8 @@
-# tracking the faces or objectes or QRcode
-import cv2
-import numpy as np
-import time
-from command import Commands
-
+# tracking the target
 class Tracker:
     def __init__(self):
-        self.errors = []       
+        self.errors = []   
+        self.center_s = 320
 
     def face_track(self , center_x , center_s , lcd_text): # tracking the face
         pass
@@ -14,13 +10,13 @@ class Tracker:
     def object_track(self , center_x , center_s , lcd_text): # tracking the object
         pass
 
-    def QR_track(self , center_x , center_s): # tracking the QRcode
-        if center_x is None or center_s is None:
+    def QR_track(self , center_x): # tracking the QRcode
+        if center_x is None:
             return None
         # if the center_x is not in the center of the screen
-        if center_x > center_s + 50: # if the center_x is in the right side of the screen
+        if center_x > self.center_s + 50: # if the center_x is in the right side of the screen
             return 'R'
-        elif center_x < center_s - 50: # if the center_x is in the left side of the screen
+        elif center_x < self.center_s - 50: # if the center_x is in the left side of the screen
             return 'L'
         else: # if the center_x is in the center of the screen
             return 'C'
