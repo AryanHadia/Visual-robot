085b36b61790d6d7afdd7263f07bf0407642e60b
AryanHadia <aryan.hd09@gmail.com>
2026-07-08 03:10:58 +0330

    Refactor run.py to use Run class for execution

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 # run the progress of the program
 from core import Core
 
-Core().run()
+class Run:
+    def __init__(self):
+        self.core = Core()
+
+    def main(self):
+        self.core.run()
+
+Run().main()
