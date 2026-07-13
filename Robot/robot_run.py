a02a62a2ed370e3fd6e75c738879b65eff6be1ea
AryanHadia <aryan.hd09@gmail.com>
2026-07-14 01:47:34 +0330

    Update robot_run.py

--- Robot/robot_run.py
+++ Robot/robot_run.py
@@ -1,146 +1,114 @@
-import socket
-import serial
-import serial.tools.list_ports
+# running the command_receive and stream and command sender to ardunino on raspi3
+import subprocess
 import time
+import signal
+import sys
+import os
+import socket
 
-# try to connect to pc for receiving commands and Arduino for sending commands
-
-class CommandReceive:
+class RunRobot:
     def __init__(self):
-        # connection attempt:
-        self.ar_attempt = 0
-        self.pc_attempt = 0
-        # try to connect to pc and arduino
-        self.arduino_is_con = False
-        self.pc_is_con = False
-        self.sock = None
-        self.arduino = None
-        # ip and port
-        self.ip = "0.0.0.0"
-        self.port = 5001
+        # camera stream
+        self.stream = None
+        self.is_streaming = False
+        self.Camera_Device = '/dev/video0' # camera device path
+        self.PYTHON_VENV = '/home/aryan/myenv/bin/python3'
+        self.stream_port = 5000
+        self.first_connect = False
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    
-    # pc connection
-    def connect(self , ip , port):
+        self.server.bind(("0.0.0.0", 5000))
+        self.server.listen(1)
+
+        print("Waiting for PC...")
+
+        self.conn, self.addr = self.server.accept()
+
+        print(f"Connected: {self.addr}")
+
+        # command receiver
+        self.COMMAND_RECEIVER_PATH = '/home/aryan/visual/command_receiver.py'
+
+        # process
+        self.cr_process = []
+
+        # signal handler
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+    def cleanup(self):
         try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.bind((ip, port))
-            self.sock.listen(1)
-            print("Waiting for client...")
-            self.conn, self.addr = self.sock.accept()
-            self.conn.settimeout(5) # 
-            print(f"connected by {self.addr}")
-            self.pc_is_con = True
-            self.pc_attempt = 0
-            return True
-        except :
-            return False
-            
-    def receive_command(self): # receive command from pc
-        """
-        receive command from pc
-        """
-        try:
-            if self.conn is not None: # if the connection is established
-                data = self.conn.recv(1024) # receive the command from pc
+            for process in self.cr_process:
+                if process.poll() is None:
+                    os.killpg(
+                    os.getpgid(process.pid),
+                    signal.SIGTERM)
+                    print(f"terminated process {process.pid}")
 
-                if not data: # if no data received
-                    return None
-
-                return data.decode().strip()
         except Exception as e:
-            print(f"error receiving command: {e}")
-            return None
+            print(f"failed to terminate process: {e}")
+        time.sleep(1) # wait for the process to terminate
+        for process in self.cr_process:
+            try:
+                if process.poll() is None:
+                    process.kill()
+            except Exception as e:
+                print(f"failed to kill process: {e}")
+                pass
+        print("all processes terminated")
+        sys.exit(0)
 
 
-    # arduino connection
-    def connect_arduino(self): # try to connect arduino with founded port
-        try:
-            ports = ['/dev/ttyACM0' , '/dev/ttyUSB0'] # Ardunino portes
-            if self.ar_attempt > 3: # if the connection attempt is more than 3 times
-                raise Exception("failed to connect to arduino after multiple attempts")
-            # try to connect to arduino
-            for port in ports:
-                try:
-                    self.arduino = serial.Serial(port, 9600, timeout=1)
-                    break
-                except:
-                    continue
-            self.arduino_is_con = True
-            self.ar_attempt = 0
-            print("connected to arduino !")
-            return
-        except Exception as e:
-            self.ar_attempt += 1
-            print(f"failed to connect to arduino: {e}")
-            if self.ar_attempt > 3:
-                raise Exception("failed to connect to arduino after multiple attempts")
-
-    def send_command(self, command): # send command to Arduino 
-        """
-        send command to Arduino
-        """
-        if self.arduino is not None:
-            self.arduino.write(command.encode())
-            print(f"sent command: {command}")
-    
-
-    # closing all connections
-    def close_all(self):
-        """
-        close all connections
-        """
-        if self.conn is not None: # closing the connection
-            self.conn.close()
-            self.conn = None
-            print("conn closed")
-        if self.sock is not None: # closing the socket
-            self.sock.close()
-            self.sock = None
-            print("sock closed")
-        if self.arduino is not None: # closing the Arduino
-            self.arduino.close()
-            self.arduino = None
-            print("arduino closed")
-    
-    # run the command receive
-    def run(self):
-        # connecting to the computer
-        self.connect(self.ip, self.port) # Connecting
-        if self.pc_is_con is True: # if connected
-            self.connect_arduino()
-            if self.arduino_is_con is True:
-                print("connected to arduino !")
-            else: # if failed to connecting to the Arduino
-                print("failed to connect to arduino !")
-                return False
-        else: # if not connected
-            print("failed to connect to pc !")
-            return False
+    def signal_handler(self, signal, frame):
+        self.cleanup()
 
 
-        print("Done !")
+    def main(self): # start the stream
+        print(f"starting stream. camera is on port {self.stream_port}") 
+        stream_cmd = [
+    "ffmpeg",
+    "-fflags", "nobuffer",
+    "-flags", "low_delay",
+    "-f", "v4l2",
+    "-framerate", "20",
+    "-video_size", "640x480",
+    "-i", "/dev/video0",
+    "-f", "mjpeg",
+    "-q:v", "5",
+    "-"
+]
+        p1 = subprocess.Popen(
+    stream_cmd,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.DEVNULL
+)
+        self.cr_process.append(p1)
+        print("stream started")
+        self.is_streaming = True
+        # command receiver
+        print("trying to run command receiver")
+        cr_cmd = f"{self.PYTHON_VENV} {self.COMMAND_RECEIVER_PATH}"
+        p2 = subprocess.Popen(
+        [self.PYTHON_VENV, self.COMMAND_RECEIVER_PATH],
+        preexec_fn=os.setsid
+        )
+        print("command receiver started")
+        self.cr_process.append(p2)
+        # wait for the processes to complete
         while True:
-            command = self.receive_command() # receive the command
-            if command is None: # if no command received
-                continue
-             
-            if command == "exit": # if the command is exit
-                self.close_all()
+            # if the stream is not streaming
+            data = p1.stdout.read(4096)
+            if not data:
                 break
 
-            print(f"received command: {command}")
-            self.send_command(command) # send command to arduino
+            # send the data to the client
+            self.conn.sendall(data)
+        p2.wait()
+        print("stream and command receiver terminated")
+        self.is_streaming = False
 
+        
 
-    # reconnect the connections
-    def reconnect(self):
-        self.close_all()
-        self.connect(self.ip, self.port)
-        if not self.arduino_is_con:
-            self.connect_arduino()
-        print("reconnected the connections")
-
-if __name__ == "__main__": # if the program is run directly
-    CommandReceive = CommandReceive()
-    CommandReceive.run()
+robot = RunRobot()
+robot.main()
