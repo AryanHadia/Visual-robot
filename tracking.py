2c2b3ef6c2b88ea467646ae37d931161de919122
AryanHadia <aryan.hd09@gmail.com>
2026-07-26 02:21:24 +0330

    Refactor Tracker class for target tracking

--- tracking.py
+++ tracking.py
@@ -4,13 +4,7 @@ class Tracker:
         self.errors = []   
         self.center_s = 320
 
-    def face_track(self , center_x , center_s , lcd_text): # tracking the face
-        pass
-
-    def object_track(self , center_x , center_s , lcd_text): # tracking the object
-        pass
-
-    def QR_track(self , center_x): # tracking the QRcode
+    def tracker (self , center_x): # tracking the target
         if center_x is None:
             return None
         # if the center_x is not in the center of the screen
