bc94572320c8a8a395b4fc9de8fd619b7b70d23c
AryanHadia <aryan.hd09@gmail.com>
2026-07-28 03:03:57 +0330

    Refactor Commands class and add state management

--- command.py
+++ command.py
@@ -3,24 +3,34 @@ import time
 import random
 from commandsender import CommandSender as CS
 from datetime import datetime
-from tracking import Tracker
 
 class Commands: 
     def __init__(self):
         self.errors = []
         self.ComSender = CS()
-        self.tracker_ = Tracker()
         self.last_command = "" # last command sent
+
+
+    def state(self , state , lcd_text=None , option=None): # change the state of the robot
+        states = {'stuck' : self.stuck , 'search' : self.search , 'sleep_mode' : self.sleep_mode , 'turn' : self.turn}
+        if state not in states:
+            return False
+        if state == 'turn':
+            return self.turn(option=option , lcd_text=lcd_text)
+        else:
+            states[state](lcd_text=lcd_text)
+        return True
+
 
     def send(self , command , lcd_text): # send the command bt self.ComSender
         try:
-            if command == self.last_command: # if the command is the same as last command
+            packet = f'{command}|{lcd_text}/\n'
+            if packet == self.last_command: # if the command is the same as last command
                 return False
-            elif command != self.last_command:
+            elif packet != self.last_command:
                 print(f"command / {lcd_text} / {command}")
-                print('1')
-            self.ComSender.send(f'{command}|{lcd_text}/\n')
-            self.last_command = command # update the last command
+            self.ComSender.send(packet)
+            self.last_command = packet # update the last command
             return True
         except Exception as e:
             error = f'command / {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} / Failed to send command | Error: {e}'
@@ -28,13 +38,13 @@ class Commands:
             self.errors.append(error)
             return False
     
+
     def stuck(self , lcd_text = None):
         # a command that will be sent to the robot when it is stuck to escape
         # make a ramdom direction to exape the robot
         direction = random.choice(['L' , 'R'])
         try:
             self.send(command=direction , lcd_text=lcd_text) # send the direction to the robot
-            time.sleep(1)
             self.send(command='S' , lcd_text=lcd_text) # send the reset command to the robot
         except Exception as e:
             error = f'command / {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} / Failed to send stuck command | Error: {e}'
@@ -43,26 +53,17 @@ class Commands:
             return False
         return True
 
-
-    def search_mode(self , lcd_text = None): # turning the servo motor to left and right
-        # turn right 20 deg
-        try:
-            for _ in range(1,10): # turn right 20 deq
-                self.send('R' , lcd_text)
-            self.send('C' , lcd_text) # recentering the servo motor
-            for _ in range(1,10): # turning left for 20 deg
-                self.send('L' , lcd_text)
-            self.send('C' , lcd_text) # recentering the servo motor
-            return True
-        except: # and if it failed
-            return False
     
     def sleep_mode(self , lcd_text = None): # sleep mode
         try:
             self.send(command='S' , lcd_text=lcd_text)
             return True
-        except:
+        except Exception as e:
+            error = f'command / {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} / Failed to send sleep mode command | Error: {e}'
+            print(error)
+            self.errors.append(error)
             return False
+
 
     def turn(self , option , lcd_text = None): # turning left and right
         if option == 'L':
@@ -72,35 +73,21 @@ class Commands:
             self.send(command='R' , lcd_text=lcd_text)
             return True
         elif option == 'C':
-            self.send(command=None , lcd_text=lcd_text)
+            self.send(command='C' , lcd_text=lcd_text)
             return True
         else:
             print('command: Invalid option')
             self.errors.append(f'command / {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} / Invalid option received')
 
+
     def search(self , lcd_text = None):
         # a command that will be sent to the robot when it is searching for the target
-        search_step = 10 # 20 deg
-        try:
-            for _ in range(search_step):
-                self.send(command='R' , lcd_text=lcd_text)
-                time.sleep(0.5)
-            self.send('C' , lcd_text) # send the recenter command to the robot
-            for _ in range(search_step):
-                self.send(command='L' , lcd_text=lcd_text)
-                time.sleep(0.5)
-            self.send(command='C' , lcd_text=lcd_text) # send the recenter command to the robot
-        except Exception as e:
-            error = f'command / {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} / Failed to send search command | Error: {e}'
-            print(error)
-            self.errors.append(error)
-            return False    
-        return True
+        self.send(command='SE' , lcd_text=lcd_text)
         
 
     def flush_error(self): # saving error in the error file
         try:
-            with open('error_log.txt', 'w') as f: # overwrite the error file
+            with open('error_log.txt', 'a') as f: # overwrite the error file
                 for error in self.errors:
                     f.write(error + '\n')
                 self.errors.clear() # clearing the error list
