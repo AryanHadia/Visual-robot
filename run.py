443e44c077191c321296fe183c46e57c4f3e8fb5
AryanHadia <aryan.hd09@gmail.com>
2026-07-29 01:35:22 +0330

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,19 @@ class Run:
         self.core = Core()
 
     def main(self):
-        self.core.run()
+        self.menu()
+        option = input("Enter your option: ").lower()
+        while option not in ["qrcode" , "face"]:
+            print("Invalid option")
+            option = input("Enter your option: ").lower()
+        self.core.run(option = option)
+
+    def menu(self):
+        print("================")
+        print("qrcode")
+        print("face")
+        print("================")
+    
+    
 
 Run().main()
