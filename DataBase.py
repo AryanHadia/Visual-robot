69bdb5eb6ed6e0040313c04f8132dcbeeb23bb65
AryanHadia <aryan.hd09@gmail.com>
2026-08-01 00:17:18 +0330

    Update DataBase.py

--- DataBase.py
+++ DataBase.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 Initialize the database
 '''
 
-engine = create_engine("mysql+mysqlconnector://aryan:pythonint12@localhost:3306/visual_db", echo=False, future=True)
+engine = create_engine("mysql+mysqlconnector://Database/visual_db", echo=False, future=True)
 Base = declarative_base()
 sessionlocal = sessionmaker(bind=engine)
 session = sessionlocal()
